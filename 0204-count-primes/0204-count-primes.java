a9a9ef9e124b59b15160e89239f84c68053544f4
UNKNOWN_UA18+ <agrawalupman@gmail.com>
2026-09-12 09:21:35 +0530

    Time: 708 ms (25.76%), Space: 79.8 MB (19.27%) - LeetHub-Neo

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPrimes(int n) {
+       int count=0;
+       boolean isPrime[]=new boolean[n];
+       Arrays.fill(isPrime,true);
+       for(int i=2;i*i<n;i++){
+        if(isPrime[i]==true){
+        for(int j=i*i;j<n;j=j+i){
+            isPrime[j]=false;
+        }}
+       }
+       for(int i=2;i<n;i++){
+        if(isPrime[i]==true)count++;
+       } 
+       return count;
+    }
+}
